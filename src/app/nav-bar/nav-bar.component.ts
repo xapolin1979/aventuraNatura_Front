b66699ceafeae317d00eadb4753f22a149eab3eb
Eloi Ginesti <ginesti1979@gmail.com>
2024-05-28 00:21:03 +0200

    aventuraNatura_front

---
+++ src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+email:string='ginesti@gmail.com';
+location:string='Sabadell (Barcelona)'
+}
