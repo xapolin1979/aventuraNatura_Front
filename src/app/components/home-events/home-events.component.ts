28e1f89b4e3052c6e24d7b6b85fc65bca2eea242
Eloi Ginesti <ginesti1979@gmail.com>
2024-05-28 23:09:57 +0200

    home

---
+++ src/app/components/home-events/home-events.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-events',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-events.component.html',
+  styleUrl: './home-events.component.css'
+})
+export class HomeEventsComponent {
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { HomeEventsComponent } from '../components/home-events/home-events.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HomeEventsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
