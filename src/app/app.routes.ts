12c25c7bc75b7903f81f28baa546d951c5b2e369
Eloi Ginesti <ginesti1979@gmail.com>
2024-05-29 22:27:01 +0200

    home acabada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { PublicEventsComponent } from './public-events/public-events.component';
 export const routes: Routes = [
-    {path:'',component:HomeComponent}
+    {path:'',component:HomeComponent},
+    {path:'events',component:PublicEventsComponent}
 
 ];
--- src/app/components/home-events/home-events.component.ts
+++ src/app/components/home-events/home-events.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { EventosGeneralService } from '../../services/eventos-general.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-home-events',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home-events.component.html',
   styleUrl: './home-events.component.css'
 })
 export class HomeEventsComponent {
 
+  datosEventos:any;
+  constructor(public eventosGeneralService:EventosGeneralService){}
+
+     ngOnInit(){
+
+      this.eventosGeneralService.eventosGeneral().subscribe(
+        response => {
+          this.datosEventos=response.data;
+          console.log(this.datosEventos);
+         
+        },
+        error => {
+          console.error(error);
+        
+        }
+      );
+     }
 }
