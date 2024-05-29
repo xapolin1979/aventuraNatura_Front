7af55511f21e7f85ba70d063c9439c1818991a3d
Eloi Ginesti <ginesti1979@gmail.com>
2024-05-29 22:27:29 +0200

    home acabada

---
+++ src/app/public-events/public-events.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-public-events',
+  standalone: true,
+  imports: [],
+  templateUrl: './public-events.component.html',
+  styleUrl: './public-events.component.css'
+})
+export class PublicEventsComponent {
+
+}
